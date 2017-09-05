082c7b3ab5d49b7d7b4a78bd84c610d6defcb387
Robert Elwell <robert.elwell@gmail.com>
2017-09-05 19:01:19 -0400

    more debugging

--- learner.py
+++ learner.py
@@ -105,6 +105,10 @@ def main():
     print("Got data, took %.2f seconds" % took1)
     print("Vectorizing features and classes...")
     X, y = build_features_and_classes()
+    print("X")
+    print(X)
+    print("y")
+    print(y)
     took2 = time.time() - took1
     print("Got data, took %.2f seconds" % took2)
     print("Building models with 10-fold cross-validation")
