56d183f3ba0219022f939711ebf419d8122ea3f3
Robert Elwell <robert.elwell@gmail.com>
2017-09-05 18:50:34 -0400

    come on man

--- learner.py
+++ learner.py
@@ -92,7 +92,7 @@ def build_features_and_classes():
         for team in sorted(GAME_TO_TEAM_POINTS[game].keys()):
             game_teams_sorted.append((game, team))
     raw_X = [game_features(game, team) for game, team in game_teams_sorted]
-    raw_Y = np.array([int(team == GAME_TO_WINNING_TEAM[game]) for game, team in game_teams_sorted], ndmin=1)
+    raw_Y = np.array([[int(team == GAME_TO_WINNING_TEAM[game])] for game, team in game_teams_sorted], ndmingit =1)
     hasher = FeatureHasher(input_type='pair')
     return hasher.fit_transform(raw_X, raw_Y).toarray(), raw_Y
 
