8f49bd0b770f45eabd980f4d2453283551de1d58
Robert Elwell <robert.elwell@gmail.com>
2017-09-06 16:48:48 -0400

    try to use alternate_sign

--- learner.py
+++ learner.py
@@ -95,7 +95,7 @@ def build_features_and_classes():
             game_teams_sorted.append((game, team))
     raw_X = [game_features(game, team) for game, team in game_teams_sorted]
     raw_Y = [int(team == GAME_TO_WINNING_TEAM[game]) for game, team in game_teams_sorted]
-    hasher = FeatureHasher(input_type='pair')
+    hasher = FeatureHasher(input_type='pair', alternate_sign=False)
     return hasher.transform(raw_X).toarray(), raw_Y
 
 
